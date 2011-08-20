b0c1e355528f1a2f2ceacccabb0725cd7f568bc5
Kolemp <przemyslaw.koltermann@wachowiakisyn.pl>
2011-08-20 11:53:58 +0200

    Fixed tree width

--- seu/menu.php
+++ seu/menu.php
@@ -9,47 +9,46 @@ if(file_exists($alert_file))
 $alert = nl2br($alert);
 ?>
 <div style="padding: 15px;">
-<div style="display: inline; float: left; padding-top: 4px;"><center>
-<a href="index.php" style="font-weight:bold; color:#000;">DRZEWO URZĄDZEŃ</a>  
-<a style="font-weight:bold; color:#000;" href="lista_ip.php">ADRESY IP</a>
-<a style="font-weight:bold; color:#000;" href="vlan_zarzadzaj.php">VLANY</a>
-<a style="font-weight:bold; color:#000;" href="mieszkania_zarzadzaj.php">ZAKRESY PRZEŁĄCZNIKÓW</a>
-<a style="font-weight:bold; color:#000;" href="historia_ip.php">HISTORIA IP</a>&nbsp&nbsp
-</div>
-<div id="menu">
-<ul id="sddm">
-    <li><a href="#" 
-        onmouseover="mopen('m2')" 
-        onmouseout="mclosetime()">URZĄDZENIA</a>
-        <div id="m2" 
-            onmouseover="mcancelclosetime()" 
-            onmouseout="mclosetime()">
-        <a style="font-weight:bold; color:#000;" href="find.php">Szukaj</a>
-        <a style="font-weight:bold; color:#000;" href="dodaj.php">Dodaj</a>
-        <a style="font-weight:bold; color:#000;" href="magazyn.php">Magazyn</a>
-        </div>
-    </li>
-    <li><a href="#" 
-        onmouseover="mopen('m1')" 
-        onmouseout="mclosetime()">ZESTAWIENIA</a>
-        <div id="m1" 
-            onmouseover="mcancelclosetime()" 
-            onmouseout="mclosetime()">
-        <a target="_blank" href="reaports/l3Switches.php" style="font-weight:bold; color:#000;" >Przełączniki Rejonowe</a>
-        <a target="_blank" href="reaports/l2Switches.php" style="font-weight:bold; color:#000;" >Przełączniki Budynkowe</a>
-        <a target="_blank" href="reaports/voipGateways.php" style="font-weight:bold; color:#000;" >Bramki voip</a>
-        <a target="_blank" href="reaports/switches.php" style="font-weight:bold; color:#000;" >Szczegółowe</a>
-        <a target="_blank" href="reaports/vlanIpUtilization.php" style="font-weight:bold; color:#000;" >Wykorzystanie adresów IP</a>
-        </div>
-    </li>
-</ul>
-</div>
-<div style="display: inline; float:right; padding-top:4px;">
-<a style="font-weight:bold; color:#000;" href="editUser.php">edytuj konto</a>
-<a style="font-weight:bold; color:#000;" href="index.php?wyloguj=true">wyloguj [<?php echo $_SESSION['user_login']; ?>]</a>
-</center>
-</div>
-</div>
+  <div style="display: inline; float: left; padding-top: 4px;"><center>
+  <a href="index.php" style="font-weight:bold; color:#000;">DRZEWO URZĄDZEŃ</a>  
+  <a style="font-weight:bold; color:#000;" href="lista_ip.php">ADRESY IP</a>
+  <a style="font-weight:bold; color:#000;" href="vlan_zarzadzaj.php">VLANY</a>
+  <a style="font-weight:bold; color:#000;" href="mieszkania_zarzadzaj.php">ZAKRESY PRZEŁĄCZNIKÓW</a>
+  <a style="font-weight:bold; color:#000;" href="historia_ip.php">HISTORIA IP</a>&nbsp&nbsp
+  </div>
+  <div id="menu">
+  <ul id="sddm">
+      <li><a href="#" 
+          onmouseover="mopen('m2')" 
+          onmouseout="mclosetime()">URZĄDZENIA</a>
+          <div id="m2" 
+              onmouseover="mcancelclosetime()" 
+              onmouseout="mclosetime()">
+          <a style="font-weight:bold; color:#000;" href="find.php">Szukaj</a>
+          <a style="font-weight:bold; color:#000;" href="dodaj.php">Dodaj</a>
+          <a style="font-weight:bold; color:#000;" href="magazyn.php">Magazyn</a>
+          </div>
+      </li>
+      <li><a href="#" 
+          onmouseover="mopen('m1')" 
+          onmouseout="mclosetime()">ZESTAWIENIA</a>
+          <div id="m1" 
+              onmouseover="mcancelclosetime()" 
+              onmouseout="mclosetime()">
+          <a target="_blank" href="reaports/l3Switches.php" style="font-weight:bold; color:#000;" >Przełączniki Rejonowe</a>
+          <a target="_blank" href="reaports/l2Switches.php" style="font-weight:bold; color:#000;" >Przełączniki Budynkowe</a>
+          <a target="_blank" href="reaports/voipGateways.php" style="font-weight:bold; color:#000;" >Bramki voip</a>
+          <a target="_blank" href="reaports/switches.php" style="font-weight:bold; color:#000;" >Szczegółowe</a>
+          <a target="_blank" href="reaports/vlanIpUtilization.php" style="font-weight:bold; color:#000;" >Wykorzystanie adresów IP</a>
+          </div>
+      </li>
+  </ul>
+  </div>
+  <div style="display: inline; float:right; padding-top:4px;">
+  <a style="font-weight:bold; color:#000;" href="editUser.php">edytuj konto</a>
+  <a style="font-weight:bold; color:#000;" href="index.php?wyloguj=true">wyloguj [<?php echo $_SESSION['user_login']; ?>]</a>
+  </center>
+  </div>
 </div>
 <div style="clear: both"></div>
 <?php
--- seu/modyfikuj.php
+++ seu/modyfikuj.php
@@ -234,10 +234,9 @@ if(isset($_POST['dodaj']))
 		}
 	break;
 	}
-        if($device['device_type']=='Virtual' && $device['lokalizacja']=='11111')
-	  echo"<center><a href=\"index.php?device=".$device['dev_id']."\">Powrót</a></center>";
-        else
-	  echo"<center><a href=\"modyfikuj.php?device=".$device['dev_id']."\">Powrót</a></center>";
+        if($device['lokalizacja']!='11111')
+	  echo"<center><a href=\"index.php?device=".$device['dev_id']."\">Drzewo urządzeń</a></center>";
+	echo"<center><a href=\"modyfikuj.php?device=".$device['dev_id']."\">Edycja</a></center>";
 }
 elseif (isset($_GET['device']))
 {
