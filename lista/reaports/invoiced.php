04a27dc22f829ad6bf9a72a7993aa046c7aabf66
Kolemp <przemyslaw.koltermann@wachowiakisyn.pl>
2012-05-09 08:21:54 +0200

    Lista renamed invoiced reaport file

--- lista/reaports/invoiced.php
+++ lista/reaports/invoiced.php
@@ -8,7 +8,7 @@ $rows = Installations::getInvoiced($date);
 if($rows)
 {
   header("Content-type: application/octet-stream");
-  header("Content-Disposition: attachment; filename=\"$date.zestawienie_montazy.csv\""); 
+  header("Content-Disposition: attachment; filename=\"$date.zestawienie_instalacji.csv\""); 
   echo DataTypes::arrayToCsv($rows, true);
 }
 else
