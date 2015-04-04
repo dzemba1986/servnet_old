1cde79f3269942a748d9c323e0d72aac21ae627b
Daniel <daniel@WWWdev.linksys.com>
2015-04-04 10:49:50 +0200

    Poprawka all host

--- seu/device_menus/switch_bud.php
+++ seu/device_menus/switch_bud.php
@@ -1,27 +1,28 @@
 <?php
 require(SEU_ABSOLUTE.'/include/classes/ipV4.php');
+
 function generateMenu($device)
 {
-	//$parent_id1 = $daddy->getDeviceModel($device['parent_device']);
-	//$parent_id = $parent_id1['id'];
-	
+	$daddy = new Daddy();
+	$parent_id1 = $daddy->getDeviceModel($device['dev_id']);
+	$parent_id = $parent_id1['id'];
+
 	if($parent_id == '46' || $parent_id == '47'){
 	
 		$menu = "<h2>Operacje</h2><ul>";
-  		$menu.="<li><a target=\"_blank\" href=\"dev/8000GS/all_hosts.php?device=".$device['dev_id']."\">Add all Hosts</a></li>";
-  		$menu.="<li><a target=\"_blank\" href=\"dev/8000GS/drop_all_hosts.php?device=".$device['dev_id']."\">Drop all Hosts</a></li>";
-  		$menu.="</ul>";
-	
+		$menu.="<li><a target=\"_blank\" href=\"dev/x210/all_hosts.php?device=".$device['dev_id']."\">Add all Hosts</a></li>";
+		$menu.="<li><a target=\"_blank\" href=\"dev/x210/drop_all_hosts.php?device=".$device['dev_id']."\">Drop all Hosts</a></li>";
+		$menu.="</ul>";
 	}
 	else{
-	
+		
 		$menu = "<h2>Operacje</h2><ul>";
-  		$menu.="<li><a target=\"_blank\" href=\"dev/x210/all_hosts.php?device=".$device['dev_id']."\">Add all Hosts</a></li>";
-  		$menu.="<li><a target=\"_blank\" href=\"dev/x210/drop_all_hosts.php?device=".$device['dev_id']."\">Drop all Hosts</a></li>";
-  		$menu.="</ul>";
+		$menu.="<li><a target=\"_blank\" href=\"dev/8000GS/all_hosts.php?device=".$device['dev_id']."\">Add all Hosts</a></li>";
+		$menu.="<li><a target=\"_blank\" href=\"dev/8000GS/drop_all_hosts.php?device=".$device['dev_id']."\">Drop all Hosts</a></li>";
+		$menu.="</ul>";
+		
 	}
-	
-  
+
   echo $menu;
 }
 
